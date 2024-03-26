2e9f6f0ff6f64ab63f1aa4c830f5cf89d6aa534b
siradawhqn@gmail.com <siradawhqn@gmail.com>
2024-03-26 22:45:13 +0700

    add docker file for project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
-import './App.css';
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
+import "./App.css";
 
 const App = () => {
   const [courseGoals, setcourseGoals] = useState([
-    {id: 'cg1', text: 'Finish the Course'},
-    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
-    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A AA" },
   ]);
-  
+
   const addNewGoalHander = (newGoal) => {
     // setcourseGoals(courseGoals.concat(newGoal));
-    setcourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal))
+    setcourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
   };
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHander}/>
+      <NewGoal onAddGoal={addNewGoalHander} />
       <GoalList goals={courseGoals} />
     </div>
   );
